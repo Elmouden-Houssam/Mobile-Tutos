47580ad585084ed847a9e2f301562c632c049284
Elmouden-Houssam <elmoudenhoussam866@gmail.com>
2026-09-25 13:22:02 +0100

    update chapter 8

--- Construire une fonctionnalité avec des objets/Tutoriel 8 Créer et manipuler un objet/backend/test-categorie.php
+++ Construire une fonctionnalité avec des objets/Tutoriel 8 Créer et manipuler un objet/backend/test-categorie.php
@@ -1,12 +1,7 @@
 <?php
-
-require_once "Categorie.php";
-
-$categorie1 = new Categorie(1, "Développement Web", "Bleu", "💻");
-$categorie2 = new Categorie(2, "Design UI/UX", "Rose", "🎨");
-
-$categorie1->afficher();
-
-echo "<br>";
-
-$categorie2->afficher();
+    require_once("./Categorie.php");
+    $cat1 = new Categorie(1,"Design UI/UX","red","iconeA");
+    $cat2 = new Categorie(2,"DEV mobile","bleu","iconeB");
+    $cat1->afficher();
+    $cat2->afficher();
+?>
